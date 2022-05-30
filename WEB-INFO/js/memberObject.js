5b10ac91ec132f9aa62c9044ef58854458dbe851
TimyaSifer <86958026+TimyaSifer@users.noreply.github.com>
2022-05-30 17:56:49 +0800

    Update memberObject.js

--- WEB-INFO/js/memberObject.js
+++ WEB-INFO/js/memberObject.js
@@ -46,8 +46,9 @@ function objFuc(jsonType) {
         for (var i in data) {
 
             let _object = new eliteObject(data[i].num, data[i].name, data[i].job, data[i].badge);
-            console.log((_object.objBadge).toString());
+            //console.log((_object.objBadge).toString());
             if ((_object.objBadge).toString() == "资格失效") {
+                console.log('1');
                 $(objArr[i]).attr('border-left', '10px solid var(--state_off)');
                 $($(objArr[i]).children()[1].children[1]).attr('color', 'var(--state_off)');
                 $($(objArr[i]).children()[1].children[1]).text('合约失效');
