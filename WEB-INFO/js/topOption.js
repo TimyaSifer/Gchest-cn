ddf6a346c6079853f17dab981d9625587efb64cf
TimyaSifer <86958026+TimyaSifer@users.noreply.github.com>
2022-05-31 15:49:17 +0800

    Update topOption.js

--- WEB-INFO/js/topOption.js
+++ WEB-INFO/js/topOption.js
@@ -7,17 +7,17 @@ class listUrl {
     }
 }
 // 官网首页
-let option_0 = new listUrl("官网首页", "Gchest-cn/index.html"); //首页
+let option_0 = new listUrl("官网首页", "/Gchest-cn/index.html"); //首页
 let option_1 = new listUrl("论坛主页", "https://www.mcbbs.net/group-1358-1.html"); //论坛首页
 
 // 社团作品
-let option_2 = new listUrl("菁英皮肤合集", "Gchest-cn/Elite/skinElite.html"); //皮肤
-let option_3 = new listUrl("菁英模型预制", "Gchest-cn/Elite/modelElite.html"); //模型
-let option_4 = new listUrl("优秀文字作品", "Gchest-cn/Elite/storyElite.html"); //文学
-let option_5 = new listUrl("体素渲染作品", "Gchest-cn/Elite/picElite.html"); //渲染
+let option_2 = new listUrl("菁英皮肤合集", "/Gchest-cn/Elite/skinElite.html"); //皮肤
+let option_3 = new listUrl("菁英模型预制", "/Gchest-cn/Elite/modelElite.html"); //模型
+let option_4 = new listUrl("优秀文字作品", "/Gchest-cn/Elite/storyElite.html"); //文学
+let option_5 = new listUrl("体素渲染作品", "/Gchest-cn/Elite/picElite.html"); //渲染
 
 // 社团成员
-let option_6 = new listUrl("在职成员档案", "Gchest-cn/Member/member.html"); //首页
+let option_6 = new listUrl("在职成员档案", "/Gchest-cn/Member/member.html"); //首页
 
 // 选项
 var topList = $(".topPageOption ul li a");
